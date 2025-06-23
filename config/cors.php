2ba96b7693bbfdce0cf63f9efe86135a2cae7e83
jewrign <aliounebadarasene929@gmail.com>
2025-06-23 18:41:37 +0000

    Modification de cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://senstock-frontend.onrender.com'],
+    'allowed_origins' => ['https://senstock-frontend.onrender.com/api'],
 
     'allowed_origins_patterns' => [],
 
